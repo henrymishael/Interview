b83e6887b6581e4ac16267bf5622231bf6be8566
henrymishael <henrymishael.hm@gmail.com>
2023-12-06 15:22:22 +0100

    fixed header

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import { ArrowDown } from 'iconsax-react'
 import chevron from '@/app/images/arrowch.svg'
 import Image from 'next/image'
@@ -6,33 +8,73 @@ import avatar from '@/app/images/avatar.png'
 import logo from '@/app/images/logo.png'
 
 export const Header = () => {
+
+    const [show, setShow] = useState(false)
+    const [show1, setShow1] = useState(false)
+    const [show2, setShow2] = useState(false)
+    const [show3, setShow3] = useState(false)
+    
+
+     const handleClick = () => {
+        setShow(!show) 
+    }
+     const handleClick1 = () => {
+        setShow1(!show1) 
+    }
+     const handleClick2 = () => {
+        setShow2(!show2) 
+    }
+     const handleClick3 = () => {
+        setShow3(!show3) 
+    }
+
   return (
     <header className='flex flex-row  justify-around  h-[80px] items-center  fixed left-0 right-0 top-0 shadow-md bg-white z-[999]'>
         {/* <Image src={logo} /> */}
         <nav className='flex flex-row gap-7 text-[14px] items-center pl-32 '> 
         <div className=' flex flex-col gap-1 pt-2 ' >
-            <h7 className=' '>Home</h7>
+            <h7 className='text-[#4a84d6] '>Home</h7>
             <span className='h-1.5 w-1.5 bg-[#4A84D6] rounded-full ml-5'></span>
         </div>
-        <div className='flex flex-row justify-center gap-2'>
+        <div className='relative flex flex-row justify-center gap-2'>
         <h7 className=' '>Solutions</h7>
-        <Image src={chevron}/>
+        <Image onClick={handleClick1} src={chevron}/>
+        <div className={`absolute w-[140px] min-h-[100px] top-14 bg-white ${show1 ? 'flex' : 'hidden'}   flex-col gap-4 text-[14px] px-3 shadow-md border-black/20 border-[1px] right-[.3rem] border-solid transition-all duration-300 ease-in-out text-[#4a84d6] font-medium`}>
+                <p className='cursor-pointer'>GoPal</p>
+                <p className='cursor-pointer'>GoSchool</p>
+                <p className='cursor-pointer'>GoFamily</p>
+            </div>
         </div>
-        <div className='flex flex-row gap-2'>
+        <div className='relative flex flex-row gap-2'>
         <h7 className=' '>Products</h7>
-        <Image src={chevron}/>
+        <Image onClick={handleClick2} src={chevron}/>
+        <div className={`absolute w-[140px] min-h-[100px] top-14 bg-white ${show2 ? 'flex' : 'hidden'}   flex-col gap-4 text-[14px] px-3 shadow-md border-black/20 border-[1px] right-[.3rem] border-solid transition-all duration-300 ease-in-out`}>
+                <p className='cursor-pointer'>Paddilife</p>
+                <p className='cursor-pointer'>Simplessia</p>
+                <p className='cursor-pointer'>Others</p>
+            </div>
         </div>
-        <div className='flex flex-row gap-2'>
+        <div className='relative flex flex-row gap-2'>
         <h7 className=' '>Company</h7>
-        <Image src={chevron}/>
+        <Image onClick={handleClick3} src={chevron}/>
+        <div className={`absolute w-[140px] min-h-[100px] top-14 bg-white ${show3 ? 'flex' : 'hidden'}   flex-col gap-4 text-[14px] px-3 shadow-md border-black/20 border-[1px] right-[.3rem] border-solid transition-all duration-300 ease-in-out`}>
+                <p className='cursor-pointer'>Blog</p>
+                <p className='cursor-pointer'>Careers</p>
+                <p className='cursor-pointer'>About Us</p>
+            </div>
         </div>
         </nav>
-        <div className='relative w-[180px]  flex flex-row items-center justify-between border-black/20 border-[0.5px] border-solid rounded-md px-2 py-1 '>
+        <div className='relative w-[180px]  flex flex-row items-center justify-between border-black/20 border-[0.5px] border-solid rounded-md px-2 py-1 group '>
             <Image className='w-[40px] h-[40px]' src={avatar} alt="" />
             <span className='flex flex-row gap-2'>
                 <p>Mishael</p>
-                <Image src={chevron} />
+                <Image onClick={handleClick} className='' src={chevron} />
             </span>
+            <div className={`absolute w-[180px] min-h-[100px] top-14 bg-white ${show ? 'flex' : 'hidden'}   flex-col gap-4 text-[14px] px-3 shadow-md border-black/20 border-[1px] right-[.3rem] border-solid transition-all duration-300 ease-in-out`}>
+                <p className='cursor-pointer'>Profile</p>
+                <p className='cursor-pointer'>Notifications</p>
+                <p className='cursor-pointer'>Settings</p>
+            </div>
         </div>
     </header>
   )
